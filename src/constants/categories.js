e542de8d215a9e971edd1e98e088c444aef8d161
DavidGordonLang <davidgordonlang@gmail.com>
2025-12-12 10:26:40 +0000

    Create categories.js

---
+++ src/constants/categories.js
@@ -0,0 +1,20 @@
+// src/constants/categories.js
+
+export const CATEGORIES = [
+  "General",
+  "Travel",
+  "Food & Drink",
+  "Shopping",
+  "Health",
+  "Work",
+  "Housing",
+  "Bureaucracy",
+  "Emergency",
+  "Social",
+  "Romantic",
+  "Sexual",
+  "Parenting",
+  "Education",
+];
+
+export const DEFAULT_CATEGORY = "General";
