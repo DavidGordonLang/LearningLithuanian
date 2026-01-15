8ceeb39e4d7faf743503024ae2fe72831d8667d7
DavidGordonLang <davidgordonlang@gmail.com>
2026-01-15 11:59:50 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ import {
 } from "./services/libraryIO";
 
 /* ============================================================================ */
-const APP_VERSION = "1.5.2-beta";
+const APP_VERSION = "1.5.3-beta";
 
 const LSK_SORT = "lt_sort_v1";
 const LSK_PAGE = "lt_page";
