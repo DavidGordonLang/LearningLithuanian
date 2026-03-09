597f5b6c64453d119ad0529146cf413167928037
DavidGordonLang <davidgordonlang@gmail.com>
2026-03-09 07:43:35 +0000

    Update ExamReadingTaskView.jsx

--- src/views/training/ExamReadingTaskView.jsx
+++ src/views/training/ExamReadingTaskView.jsx
@@ -134,6 +134,8 @@ export default function ExamReadingTaskView({ onBack }) {
         </div>
       </div>
 
+      <KeywordBlock items={item?.support?.keywords} />
+
       <div className="mt-4 space-y-3">
         {questions.map((q, qIdx) => {
           const picked = answers[q.id];
@@ -217,8 +219,6 @@ export default function ExamReadingTaskView({ onBack }) {
           </div>
         </div>
       ) : null}
-
-      <KeywordBlock items={item?.support?.keywords} />
 
       <div className="mt-4 flex flex-wrap gap-3">
         {!submitted ? (
